62511f6223d96834bbacf2a3d0f90f5ce8cd072e
Javier Arias <javier.arias@pixelatom.com>
2013-03-26 16:55:05 -0300

    fixed example

--- example/index.php
+++ example/index.php
@@ -1,15 +1,12 @@
 <?php
 // includes ClassLoader implementation from the framework
-require_once '../../src/linxphp/common/ClassLoader.php';
+require_once '../vendor/autoload.php';
 
-// Autoloading configuration. Path needs to point to linxphp src folder
-$classLoader = new linxphp\common\ClassLoader('linxphp', "../../src");
-$classLoader->register();
 
 // enabled to stop at any error
-linxphp\common\ErrorHandler::register();
+//linxphp\common\ErrorHandler::register();
 
-use linxphp\http\rest\Router;
+use Pixelatom\Router;
 
 // optional and required section arguments detection
 Router::register('GET', '/users/?/*', function($action,$id=''){
